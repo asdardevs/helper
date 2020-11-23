c6c02fefc36bd51e58b96957b944cea549b77720
asdarmldnn <53163040+asdarmldnn@users.noreply.github.com>
2020-11-23 13:00:24 +0800

    Create hari_indonesia.php
    format hari bahasa indonesia

---
+++ hari_indonesia.php
@@ -0,0 +1,28 @@
+ function hari($day) {
+        $hari = $day;
+  
+        switch ($hari) {
+            case "Sun":
+                $hari = "Minggu";
+                break;
+            case "Mon":
+                $hari = "Senin";
+                break;
+            case "Tue":
+                $hari = "Selasa";
+                break;
+            case "Wed":
+                $hari = "Rabu";
+                break;
+            case "Thu":
+                $hari = "Kamis";
+                break;
+            case "Fri":
+                $hari = "Jum'at";
+                break;
+            case "Sat":
+                $hari = "Sabtu";
+                break;
+        }
+        return $hari;
+    }
